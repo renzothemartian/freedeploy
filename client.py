dd985a69dac20afb97cbbdc385bd7945aaf6a4f6
ctaggart27 <ctaggart27@gmail.com>
2021-05-07 18:54:07 -0600

    made python read and print response from a ur

--- client.py
+++ client.py
@@ -1,7 +1,14 @@
-x = input()
-import urllib2
-response = urllib2.urlopen(server)
-data = response.read()
-print(data)
-# with open('data.txt', 'r') as inps:
-#     print(inps.read())
+import sys
+import urllib
+import urllib.request
+
+# Python Version Check
+pythonVersion = sys.version_info[0]
+if pythonVersion >= 3:
+    print(f"Running Py3: {pythonVersion}")
+    #  URL PARSING
+    # LINK: https://stackoverflow.com/questions/6594620/python-3-2-unable-to-import-urllib2-importerror-no-module-named-urllib2
+    server = "https://robotrenzo.com"
+    wp = urllib.request.urlopen(server)
+    pw = wp.read()
+    print(pw)
