011ed9004c3d38aa5f791e40c4ecf34d6ef82bb4
ctaggart27 <ctaggart27@gmail.com>
2021-05-07 19:10:49 -0600

    Christian is commiting something on git

--- client.py
+++ client.py
@@ -1,14 +1,27 @@
 import sys
 import urllib
 import urllib.request
+import json
 
+url_prefix = "http://"
+file_name = "client_instructions.json"
 # Python Version Check
 pythonVersion = sys.version_info[0]
 if pythonVersion >= 3:
     print(f"Running Py3: {pythonVersion}")
+
+data = ""
+def parse_instructions():
     #  URL PARSING
     # LINK: https://stackoverflow.com/questions/6594620/python-3-2-unable-to-import-urllib2-importerror-no-module-named-urllib2
-    server = "https://robotrenzo.com"
-    wp = urllib.request.urlopen(server)
+    server_address = url_prefix + "192.168.1.112:3000" + "/" + file_name
+
+    wp = urllib.request.urlopen(server_address)
     pw = wp.read()
-    print(pw)
+    data = json.loads(pw)
+
+    print(data["client_instructions"]["date"])
+    # print(pw)
+
+
+parse_instructions()
